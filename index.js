ba8a290ab2d2dd8fd8d1df4609b9f4168e0eb14d
Cherry Ramatis <cherry.ramatis@icloud.com>
2023-09-12 19:43:09 -0300

    Initial Commit

---
+++ index.js
@@ -0,0 +1,23 @@
+"use strict";
+
+const express = require('express')
+const mustacheExpress = require('mustache-express');
+
+const app = express()
+
+app.set('views', `${__dirname}/views`);
+app.set('view engine', 'mustache');
+app.engine('mustache', mustacheExpress());
+
+app.get('/', async function(_req, res) {
+  res.setHeader("content-type", "image/svg+xml; charset=utf-8");
+  res.setHeader("cache-control", "no-cache, max-age=0");
+  const response = await fetch('https://dev.to/api/articles?username=cherryramatis')
+  const data = await response.json()
+
+  res.render('articles', { articles: data })
+});
+
+const port = process.env.NODE_PORT || 3000;
+
+app.listen(port, () => console.log(`Server running on ${port}`));
