cd99b2b80c0a7f98ed4f926306041ffc3ae77158
Cherry Ramatis <cherry.ramatis@icloud.com>
2023-09-12 20:04:50 -0300

    uau 2

--- app.js
+++ app.js
@@ -9,18 +9,13 @@ app.set('views', `${__dirname}/views`);
 app.set('view engine', 'mustache');
 app.engine('mustache', mustacheExpress());
 
-app.get('/api/articles', async function(_req, res) {
+app.get("/", async (_req, res) => {
   res.setHeader("content-type", "image/svg+xml; charset=utf-8");
   res.setHeader("cache-control", "no-cache, max-age=0");
   const response = await fetch('https://dev.to/api/articles?username=cherryramatis')
   const data = await response.json()
 
   res.render('articles', { articles: data })
-});
-
-// Server stuff
-app.get("/api/", (request, response) => {
-  response.status(200).json({ message: "Server's up" });
 });
 
 const port = process.env.PORT || 3000;
