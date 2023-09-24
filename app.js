16a762f8572b2c8949a65821dde060438e871d15
Cherry Ramatis <cherry.ramatis@icloud.com>
2023-09-24 19:34:15 -0300

    add 100 days of code visualization

--- app.js
+++ app.js
@@ -28,6 +28,24 @@ app.get("/stats", async (_req, res) => {
   }
 });
 
+app.get("/100diasdecodigo", (req, res) => {
+  const finishedDays = 72
+  const name = req.query.username;
+
+  res.setHeader("content-type", "image/svg+xml; charset=utf-8");
+  res.setHeader("cache-control", "no-cache, max-age=0");
+
+  res.render('100diasdecodigo', {
+    finishedDays,
+    daysFinishedPercentage: (finishedDays / 100) * 250,
+    name,
+    totalLikes: 8000,
+    totalViews: 180000,
+    totalReplies: 20000,
+    maxStreak: 18
+  })
+})
+
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => console.log(`Server running on ${port}`));
